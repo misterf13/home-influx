9674735756151ddc899995614c31a854c2551e4a
fgarcia13 <fgarcia1337@gmail.com>
2019-05-31 07:21:29 -0500

    Now we get used satellites as well.
    Caution: You need a modified version of python-gpsdshm.

--- gpsd/gpsd_shm_stats.py
+++ gpsd/gpsd_shm_stats.py
@@ -6,18 +6,24 @@ import gpsdshm
 def get_shm():
   return gpsdshm.Shm()
 
-def get_sats(gpsd_shm):
+def get_visible_sats(gpsd_shm):
   return gpsd_shm.satellites_visible
+
+def get_used_sats(gpsd_shm):
+  return gpsd_shm.satellites_used
 
 def get_fix(gpsd_shm):
   return gpsd_shm.fix.mode
 
-def main(satellites, gps_fix):
+def main(sats, sats_used, gps_fix):
   gpsd_shm = get_shm()
   # GPSD has to be up
-  if gpsd_shm.status and satellites:
-    sats = get_sats(gpsd_shm)
+  if gpsd_shm.status and sats:
+    sats = get_visible_sats(gpsd_shm)
     print('sats_vis={0}'.format(sats))
+  elif gpsd_shm.status and sats_used:
+    sats_used = get_used_sats(gpsd_shm)
+    print('sats_used={0}'.format(sats_used))
   elif gpsd_shm.status and gps_fix:
     fix = get_fix(gpsd_shm)
     print('fix={0}'.format(fix))
@@ -25,6 +31,7 @@ def main(satellites, gps_fix):
 if __name__ == '__main__':
   parser = OptionParser(usage='%prog [-s] [-f]', version='%prog 0.1')
   parser.add_option('-s', '--satellites', action='store_true')
+  parser.add_option('-u', '--used', action='store_true')
   parser.add_option('-f', '--fix', action='store_true')
   (options, args) = parser.parse_args()
-  main(options.satellites, options.fix)
+  main(options.satellites, options.used, options.fix)
