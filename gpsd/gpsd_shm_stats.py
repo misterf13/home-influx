73447626a5a66487f5abf2aa9daaf952cf95e6ff
fgarcia13 <fgarcia1337@gmail.com>
2019-06-01 18:54:53 -0500

    Switch libraries

--- gpsd/gpsd_shm_stats.py
+++ gpsd/gpsd_shm_stats.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python3
 from optparse import OptionParser
 
-import gpsdshm
+import shmgpsd
+import json
 
 def get_shm():
-  return gpsdshm.Shm()
+  return shmgpsd.SHM()
 
-def get_visible_sats(gpsd_shm):
-  return gpsd_shm.satellites_visible
+def get_visible_sats(shm_gpsd):
+  return shm_gpsd.satellites_visible
 
-def get_used_sats(gpsd_shm):
-  return gpsd_shm.satellites_used
+def get_used_sats(shm_gpsd):
+  return shm_gpsd.satellites_used
 
-def get_fix(gpsd_shm):
-  return gpsd_shm.fix.mode
+def get_fix(shm_gpsd):
+  return shm_gpsd.fix.mode
 
-def main(sats, sats_used, gps_fix):
-  gpsd_shm = get_shm()
-  # GPSD has to be up
-  if gpsd_shm.status and sats:
-    sats = get_visible_sats(gpsd_shm)
-    print('sats_vis={0}'.format(sats))
-  elif gpsd_shm.status and sats_used:
-    sats_used = get_used_sats(gpsd_shm)
-    print('sats_used={0}'.format(sats_used))
-  elif gpsd_shm.status and gps_fix:
-    fix = get_fix(gpsd_shm)
-    print('fix={0}'.format(fix))
+def get_satellites(shm_gpsd):
+  sat_list = []
+  for sat_i in range(0, shmgpsd.MAXCHANNELS):
+    if shm_gpsd.skyview[sat_i].PRN != 0:
+      sat_list.append( {'sat':  shm_gpsd.skyview[sat_i].PRN,
+                                'snr':  shm_gpsd.skyview[sat_i].ss,
+                                'used': shm_gpsd.skyview[sat_i].used})
+  return sat_list
+
+def main():
+  gpsd_dict = {}
+  shm_gpsd  = get_shm()
+  sats      = get_visible_sats(shm_gpsd)
+  sats_used = get_used_sats(shm_gpsd)
+  fix       = get_fix(shm_gpsd)
+  gpsd_dict['fix']          = fix
+  gpsd_dict['sats_visible'] = sats
+  gpsd_dict['sats_used']    = sats_used
+  #gpsd_dict['satellites']   = get_satellites(shm_gpsd)
+
+  print(json.dumps(gpsd_dict, indent=2))
 
 if __name__ == '__main__':
-  parser = OptionParser(usage='%prog [-s] [-f]', version='%prog 0.1')
-  parser.add_option('-s', '--satellites', action='store_true')
-  parser.add_option('-u', '--used', action='store_true')
-  parser.add_option('-f', '--fix', action='store_true')
-  (options, args) = parser.parse_args()
-  main(options.satellites, options.used, options.fix)
+  main()
